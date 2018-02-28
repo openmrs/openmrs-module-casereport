4a43be38bb41296508ceb4de5d56b60edf19b273
wyclif <wyclif@openmrs.org>
2018-02-27 19:50:16 -0600

    Added support for enabling CORS for the CBR simulator to access the rest API inside the browser

---
+++ omod/src/main/java/org/openmrs/module/casereport/CrossOriginResourceSharingFilter.java
@@ -0,0 +1,73 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.casereport;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openmrs.api.context.Context;
+import org.springframework.stereotype.Controller;
+
+/**
+ * This filter
+ * 
+ * @see <a href="http://www.w3.org/TR/cors/">CORS specification</a>
+ */
+@Controller
+public class CrossOriginResourceSharingFilter implements Filter {
+	
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+	
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+	    throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		String gpValue = Context.getAdministrationService().getGlobalProperty(CaseReportWebConstants.GP_ENABLE_CORS);
+		if (Boolean.valueOf(gpValue)) {
+			HttpServletResponse response = (HttpServletResponse) servletResponse;
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			response.addHeader("Access-Control-Allow-Methods", "GET, POST");
+			response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+			response.addHeader("Access-Control-Max-Age", "3600");
+			
+			//TODO add GP to disable this filter
+			if (request.getMethod().equals("OPTIONS")) {
+				response.setStatus(HttpServletResponse.SC_OK);
+				
+				return;
+			}
+		}
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, servletResponse);
+	}
+	
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+	
+}
